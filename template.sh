d9a15cb5e9562718d3f7ebecca7b6d8eee0ce4ca
faja <mcabaj@gmail.com>
2023-07-27 09:41:41 +0200

    template.sh updated

--- template.sh
+++ template.sh
@@ -4,27 +4,41 @@ set -e
 # set -o pipefail
 # set -x
 
-# TODO add colors
-
 PROG_VERSION=1.0.0
 PROG_NAME=$(basename "${0}")
+# PROG_PATH=$(dirname "${0}") # if needed
+# REPO_ROOT="${PROG_PATH}"/.. # if needed
 
+#------------------------------------------------------------------------------#
+# {{{ colors
+# if needed
+# BOLD=$(tput bold)
+# RED=$(tput setaf 1)
+# YELLOW=$(tput setaf 3)
+# NORMAL=$(tput sgr0)
+# }}}
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
 # {{{ print_help()
 print_help() {
   echo "usage:   ${PROG_NAME} ARG1 ARG2 [OPTIONS]"
   echo "example: ${PROG_NAME} arg1 arg2 --bool-flag-example --value-flag-example 0.1.0"
   echo
   echo OPTIONS:
-  echo "  --help                  print this message"
-  echo "  --verbose               be more verbose (default: false)"
-  echo "  --yes                   do not ask for confirmation (default: false)"
-  echo "  --bool-flag-example     set something (default: false)"
-  echo "  --value-flag-example    set something (default: some_default)"
+  echo "  --help|-h                  print this message"
+  echo "  --verbose|-v               be more verbose (default: false)"
+  echo "  --yes|-y                   do not ask for confirmation (default: false)"
+  echo "  --bool-flag-example|-b     set something (default: false)"
+  echo "  --value-flag-example|-x    set something (default: some_default)"
   echo
   echo "VERSION: ${PROG_VERSION}"
   exit 0
 }
 # }}}
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
 # {{{ ask_continue()
 ask_continue() {
   if test -z "${YES}"; then
@@ -33,6 +47,9 @@ ask_continue() {
   fi
 }
 # }}}
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
 # {{{ verbose()
 verbose() {
   if test "${VERBOSE}"; then
@@ -40,13 +57,19 @@ verbose() {
   fi
 }
 # }}}
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
 # {{{ defaults
 BOOL_FLAG_EXAMPLE=
 VALUE_FLAG_EXAMPLE=some_default
 # }}}
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
 # {{{ option parsing
-while test $# -gt 0; do
-  case $1 in
+while test "${#}" -gt 0; do
+  case "${1}" in
     # {{{ help, yes, verbose
     --help)
       print_help
@@ -57,7 +80,13 @@ while test $# -gt 0; do
     --yes)
       YES=true
       ;;
+    -y)
+      YES=true
+      ;;
     --verbose)
+      VERBOSE=true
+      ;;
+    -v)
       VERBOSE=true
       ;;
     # }}}
@@ -65,8 +94,15 @@ while test $# -gt 0; do
     --bool-flag-example) # example of bool flag
       BOOL_FLAG_EXAMPLE=true
       ;;
+    -b) # example of bool flag (short)
+      BOOL_FLAG_EXAMPLE=true
+      ;;
     --value-flag-example)
-      VALUE_FLAG_EXAMPLE=${2}
+      VALUE_FLAG_EXAMPLE="${2}"
+      shift
+      ;;
+    -x)
+      VALUE_FLAG_EXAMPLE="${2}"
       shift
       ;;
     # }}}
@@ -83,24 +119,29 @@ while test $# -gt 0; do
       #: "${ARG1:=${1}}"
 
       ## case for two ARGs
-      if test -z "${ARG1}"; then
-        : "${ARG1:=${1}}"
-      else
-        : "${ARG2:=${1}}" # same rule apply here in terms of take FIRST vs LAST
+      if test "${ARG1}"; then # if ARG1 is already set, lets set ARG2
+        : "${ARG2:=${1}}"     # FIRST vs LAST appearance rules, same as above
       fi
+      : "${ARG1:=${1}}" # yes, this is ARG1, set it if it is not set yet
       ;;
       # }}}
   esac
   shift
 done
 # }}}
+#------------------------------------------------------------------------------#
 
-# check if all arguments were passed
+#------------------------------------------------------------------------------#
+# {{{ check if all arguments are set
 if test -z "${ARG1}" \
   || test -z "${ARG2}"; then
   print_help
 fi
+# }}}
+#------------------------------------------------------------------------------#
 
+#------------------------------------------------------------------------------#
+# {{{ main logic
 verbose
 ask_continue
 
@@ -110,3 +151,5 @@ echo ARG1: "${ARG1}"
 echo ARG2: "${ARG2}"
 echo BOOL_FLAG_EXAMPLE: "${BOOL_FLAG_EXAMPLE}"
 echo VALUE_FLAG_EXAMPLE: "${VALUE_FLAG_EXAMPLE}"
+# }}}
+#------------------------------------------------------------------------------#
